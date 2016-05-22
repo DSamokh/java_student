c1663846ff5adaea5e980aa6b45e6a9675a24fda
Denis SK <bigzeb@mail.ru>
2016-05-22 10:14:10 +0300

    Add svetofor

---
+++ svetofor/Main.java
@@ -0,0 +1,43 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+/*
+* */
+public class Main {
+
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore();
+        BufferedReader reader = null;
+
+        String query = "";
+        try {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            query = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader!=null) reader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(semaphore.getColor(Integer.parseInt(query)));
+    }
+
+    public static class Semaphore {
+        int[] interval = {2, 3, 5};
+        String[] color = {"Зеленый", "Желтый", "Красный"};
+
+        String getColor(int minutes) {
+            int summInterval = interval[0] + interval[1] + interval[2];
+            int ostatok = minutes % summInterval;
+            if (ostatok <= interval[0]) return color[0];
+            if (ostatok >= interval[1] + interval[2]) return color[2];
+            return color[1];
+        }
+    }
+
+}
+
